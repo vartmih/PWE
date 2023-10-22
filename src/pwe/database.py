3ec5935fca31ae39877344215a6599c617a91e5f
Vartikyan Mikhail <m.vartikyan@bk.ru>
2023-10-22 20:54:11 +0300

    Обновление настроек подключения к базе данных

--- src/pwe/database.py
+++ src/pwe/database.py
@@ -1,15 +1,52 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from asyncio import current_task
+from collections.abc import AsyncGenerator
 
-from .dependencies import get_db_settings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, async_scoped_session
 
-db_settings = get_db_settings()
+from pwe.settings import settings
 
-DATABASE_URL = f"postgresql://{db_settings.username}:{db_settings.password}@" \
-               f"{db_settings.host}:{db_settings.port}/{db_settings.db_name}"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class DatabaseHelper:
+    """Класс для работы с базой данных"""
 
-Model = declarative_base()
+    def __init__(self, db_url: str, debug: bool) -> None:
+        """
+        Инициализация класса
+
+        Parameters:
+            db_url: URL для подключения к базе данных
+            debug: режим отладки
+
+        :return: None
+        """
+        self.engine = create_async_engine(db_url, echo=debug)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False
+        )
+
+    def get_scoped_session(self):
+        """Возвращает сессию для работы с базой данных"""
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession, None]:
+        """
+        Генератор сессий
+
+        yield: сессия
+        """
+        with self.get_scoped_session() as session:
+            yield session
+            await session.remove()
+
+
+db_helper = DatabaseHelper(
+    db_url=settings.get_db_url(),
+    debug=settings.debug
+)
