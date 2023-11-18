c24b0aed1b8d45500b2cfd27d2285baca5db5c7f
Vartikyan Mikhail <m.vartikyan@bk.ru>
2023-11-18 23:03:53 +0300

    Рефакторинг

--- src/pwe/api_v1/user/services.py
+++ src/pwe/api_v1/user/services.py
@@ -3,10 +3,10 @@ from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from pwe.api_v1.user.models import User
-from pwe.database.db_helper import db_helper
+from pwe.database.db_helper import async_session
 
 
-async def get_user_db(session: AsyncSession = Depends(db_helper.session_dependency)):
+async def get_user_db(session: AsyncSession = Depends(async_session)):
     """
     Возвращает адаптер для работы с базой данных
 
